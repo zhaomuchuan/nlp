8f0f87ed45ed7ff261fd27ba2e6e7159fa791223
CN1294995 <junling.zhao@t-systems.com>
2020-06-29 14:39:38 +0800

    improve

--- mysplit.py
+++ mysplit.py
@@ -14,12 +14,10 @@ import pandas as pd
 import csv
 from scipy.spatial.distance import cdist
 
-fp = open('input.csv','r')
-reader = csv.reader(fp)
-lst = [i[0] for i in reader]
-
 # 构造特殊句子锁定最佳cluster part1
-lst.append('fuck in the hell~')
+df = pd.read_csv('input.csv')
+df = df.append([{'Time':'','Host':'','Problem':'fuck in the hell~'}], ignore_index=True)
+lst = df['Problem'].tolist()
 
 # 分句
 # X_Org = sent_tokenize(lst,'english')
@@ -37,20 +35,23 @@ n_target = int(len(weight)/5)
 for n in range(n_target-3, n_target+3):
     kmeans = KMeans(n_clusters=n).fit(weight)
     labels = kmeans.labels_
-    result = DataFrame({'category':labels,'sentence':lst})
+    pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
+    # copy与=区别
+    result = df.copy()
+    result.insert(0, 'category', labels)
 
     # 构造特殊句子锁定最佳cluster part2
-    a1 = result.query('sentence== "fuck in the hell~"')['category'].iat[0]
+    a1 = result.query('Problem== "fuck in the hell~"')['category'].iat[0]
     if len(result[result.category == a1]) < 2:
         print(n)
         print(result.sort_values(by='category'))
         break
-
-# groups方式输出
-# grouped = result.groupby('category')
-# for name,group in grouped:
-#     print (name)
-#     print (group.iloc[0])
-
-
+#
+# # groups方式输出
+# # grouped = result.groupby('category')
+# # for name,group in grouped:
+# #     print (name)
+# #     print (group.iloc[0])
+#
+#
